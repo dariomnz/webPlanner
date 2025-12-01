06ebce11defc2ea485a93055c7c6bd125aeabbe7
dariomnz <dario.mnz.mnz@gmail.com>
2025-12-01 19:40:36 +0100

    Add ballerina icon

--- src/components/ClassPlanner.tsx
+++ src/components/ClassPlanner.tsx
@@ -2,6 +2,8 @@
 import ClassPlannerList from './ClassPlannerList.tsx';
 import { PlannedExercise } from '../types';
 import { Trash2 } from 'lucide-react';
+import BalletIcon from '../assets/ballerina.svg?react';
+
 
 interface ClassPlannerProps {
     plannedExercises: PlannedExercise[];
@@ -14,7 +16,12 @@ export default function ClassPlanner({ plannedExercises, onRemoveExercise, onCle
         <div className="flex-1 h-screen bg-beige-50 p-8 overflow-y-auto">
             <div className="max-w-3xl mx-auto">
                 <header className="mb-8 text-center relative">
-                    <h1 className="text-4xl font-serif text-pink-950 mb-2 font-bold">Planificación de Clase</h1>
+
+                    <h1 className="text-4xl font-serif text-pink-950 mb-2 font-bold flex items-center justify-center gap-2">
+                        <BalletIcon className="fill-pink-200 size-24 inline-block" />
+                        Planificación de Clase
+                        <BalletIcon className="fill-pink-200 scale-x-[-1] size-24 inline-block" />
+                    </h1>
                     <p className="text-pink-800/80 font-medium mb-4">Arrastra ejercicios aquí para construir tu clase</p>
 
 
