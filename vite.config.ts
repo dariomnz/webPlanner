a42d18f1af248a96d87f8476da34fa9639cebaa9
Dariomnz <dario.mnz.mnz@gmail.com>
2026-01-09 20:48:02 +0100

    Add react scan

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr';
+import reactScan from 'vite-plugin-react-scan';
 
 // https://vite.dev/config/
 export default defineConfig({
     base: '/webPlanner/',
     plugins: [
+        reactScan(),
         svgr(),
         react({
             babel: {
